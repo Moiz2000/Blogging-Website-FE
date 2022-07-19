b9be9c1f9fe7f042a6c64c4659de58fa3525a563
tayyaba tabassum <tayyaba0343@gmail.com>
2022-07-19 18:16:17 +0500

    19-07-22

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import HeaderTag from './Components/HeaderTag';
 import HomeTag from './Components/Home';
 import About from './Components/About';
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import Divider from './Components/Divider';
+import Services from './Components/Services';
+import {BrowserRouter as Router, Route, Switch, Routes } from "react-router-dom";
+import Login from './Components/Login';
 
 class App extends Component {
   render() {
@@ -12,15 +14,14 @@ class App extends Component {
       <div className="App">
        <HeaderTag/>
        <HomeTag/>
-       <About/>
-        <Router>
-         <Routes>
-          {/* <Route exact path="/LogIn.js" Component={LogIn.js} /> */}
-          {/* <Route exact path="/LogIn" Component={LogIn} /> */}
-          </Routes>
-
-         </Router> 
-
+       <Divider/>
+       <About/> 
+       <Services/>
+  {/* <Router>
+    <Routes>
+      <Route path="/login" element={<Login />}></Route>
+    </Routes>
+  </Router> */}
       </div>
     );
   }
