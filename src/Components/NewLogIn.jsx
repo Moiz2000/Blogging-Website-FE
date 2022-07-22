4d1ef0aed24bc9b46b7b9d3edc4c3019dc90df0f
sawera-rehman <103102099+sawera-rehman@users.noreply.github.com>
2022-07-22 14:15:54 +0500

    Add files via upload

--- src/Components/NewLogIn.jsx
+++ src/Components/NewLogIn.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Link} from 'react-router-dom';
+import {userInfo} from './UserData';
 import './LogInCss.css';
 
 // import { isMobile } from "react-device-detect";
 // import { Box, Heading, Form, TextInput, Button, Text } from "grommet";
 
 const NewLogin = () => {
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [allEntry,setAllEntry]=useState(userInfo);
+    const SubmitForm=(e)=>{
+        e.preventDefault();
+        const newEntry={email:email,username:username};
+        localStorage.setItem("email", email)
+        localStorage.setItem("username",username)
+        setAllEntry([...allEntry, newEntry]);
+    }
     return (
         <div className='LogInmain'>
+        <button className='Backbtn'>Go Back</button>
         <div className='heading'>LogIn to Account</div>
-        <form action='/'>
-        <input className='inputEmail' type='email' placeholder='Enter your Email here' required/>
-        <input className='inputUsername' type="text" placeholder='Enter your Username here' required/>
+        <form action='' onSubmit={SubmitForm}>
+        <input className='inputEmail' type='email' name="email" placeholder='Enter your Email here' value={email} required
+        onChange={(e)=>setEmail(e.target.value)}
+        />
+        <input className='inputUsername' type="text" name="username" placeholder='Enter your Username here' value={username} 
+        onChange={(e)=>setUsername(e.target.value)}
+        required/>
         <input className='RememberCheck' type='checkbox'/>
         <p className='RememberText'>Remember Me</p>
         <p className='Forget'>Forget Password?</p>
